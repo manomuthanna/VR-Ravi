c7067c5a6a43d869099b6fd6f2f8df61d0185283
Muthanna Rajadurai <107006718+manomuthanna@users.noreply.github.com>
2022-07-27 16:29:21 +0300

    Update index.js

--- sm4-filemanager-frontend/api/GetRoles/index.js
+++ sm4-filemanager-frontend/api/GetRoles/index.js
@@ -1,5 +1,5 @@
-//const fetch = require('node-fetch').default;
-import fetch from 'node-fetch';
+const fetch = require('node-fetch').default;
+//import fetch from 'node-fetch';
 
 // add role names to this object to map them to group ids in your AAD tenant 
 const roleGroupMappings = {
